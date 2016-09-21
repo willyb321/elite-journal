06433a7de95731534f25c4befe5bd834cc5737f1
willyb321 <wbwilliam7@gmail.com>
2016-09-21 20:15:57 +1000

    cool stuff and things

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict';
+const fs = require('fs');
 const electron = require('electron');
 const tableify = require('tableify');
 const {dialog} = require('electron');
 const LineByLineReader = require('line-by-line');
-const fs = require('fs');
 
 const app = electron.app;
-var file;
+let file;
 // adds debug features like hotkeys for triggering dev tools and reload
 require('electron-debug')();
 
@@ -24,31 +24,31 @@ function createMainWindow() {
 		width: 600,
 		height: 400
 	});
-	file = dialog.showOpenDialog({properties: ['openFile', 'multiSelections']})
+	file = dialog.showOpenDialog({properties: ['openFile', 'multiSelections']});
 
 	win.on('closed', onClosed);
 	const lr = new LineByLineReader(file[0]);
 
-lr.on('error', err => {
-	return console.log(err);
-});
-
-lr.on('line', line => {
-	const test = JSON.parse(line);
-	const html = tableify(test);
-
-	fs.appendFile(`${__dirname}/index2.html`, html, err => {
-		if (err) {
-			return console.log(err);
-		}
-		console.log('The file was saved!');
+	lr.on('error', err => {
+		return console.log(err);
 	});
-});
 
-lr.on('end', () => {
-	console.log('done!');
-	win.loadURL(`file://${__dirname}/index2.html`);
-});
+	lr.on('line', line => {
+		const test = JSON.parse(line);
+		const html = tableify(test);
+
+		fs.appendFile(`${__dirname}/index2.html`, html, err => {
+			if (err) {
+				return console.log(err);
+			}
+			console.log('The file was saved!');
+		});
+	});
+
+	lr.on('end', () => {
+		console.log('done!');
+		win.loadURL(`file://${__dirname}/index2.html`);
+	});
 	return win;
 }
 
@@ -56,11 +56,11 @@ app.on('window-all-closed', () => {
 	if (process.platform !== 'darwin') {
 		app.quit();
 	}
-	fs.writeFile('./index2.html', "", err => {
+	fs.writeFile('./index2.html', '', err => {
 		if (err) {
 			return console.log(err);
 		}
-	})
+	});
 });
 
 app.on('activate', () => {
@@ -71,6 +71,4 @@ app.on('activate', () => {
 
 app.on('ready', () => {
 	mainWindow = createMainWindow();
-
 });
-
