ef3febf90c121e5c6e5963b754ace4b4ef33b859
willyb321 <wbwilliam7@gmail.com>
2016-09-24 12:56:10 +1000

    added menu, save/load, fixed css in dev, plus probably more

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
-const fs = require('fs');
+const fs = require('fs.extra');
 const electron = require('electron');
 const tableify = require('tableify');
 const {dialog} = require('electron');
@@ -13,7 +13,6 @@ require('electron-debug')();
 
 // prevent window being garbage collected
 let mainWindow;
-
 function onClosed() {
 // dereference the window
 // for multiple windows store them in an array
@@ -37,7 +36,7 @@ function createMainWindow() {
 	lr.on('line', line => {
 		const lineParse = JSON.parse(line);
 		JSONParsed.push(lineParse);
-		// console.log(JSONParsed);
+// console.log(JSONParsed);
 		const html = tableify(lineParse) + '<hr>';
 		fs.appendFile(`${process.resourcesPath}/index2.html`, html, err => {
 			if (err) {
@@ -45,7 +44,7 @@ function createMainWindow() {
 			}
 		});
 	});
-	fs.appendFile(`${process.resourcesPath}/index2.html`, '<script src="https://use.fontawesome.com/a39359b6f9.js"></script><link rel="stylesheet" href="./app.asar/index.css"><link href="https://fonts.googleapis.com/css?family=Lato:400,400italic,700" rel="stylesheet" type="text/css">', err => {
+	fs.appendFile(`${process.resourcesPath}/index2.html`, '<script src="https://use.fontawesome.com/a39359b6f9.js"></script><style>body {padding: 0; margin: 0; } body {background-color: #313943; color: #bbc8d8; font-family: \'Lato\'; font-size: 22px; font-weight: 500; line-height: 36px; margin-bottom: 36px; text-align: center; } header {position: absolute; width: 500px; height: 250px; top: 50%; left: 50%; margin-top: -125px; margin-left: -250px; text-align: center; } header h1 {font-size: 60px; font-weight: 100; margin: 0; padding: 0; } #grad {background: -webkit-linear-gradient(left, #5A3F37 , #2C7744); /* For Safari 5.1 to 6.0 */ background: -o-linear-gradient(right, #5A3F37 , #2C7744); /* For Opera 11.1 to 12.0 */ background: -moz-linear-gradient(right,#5A3F37 , #2C7744); /* For Firefox 3.6 to 15 */ background: linear-gradient(to right, #5A3F37 , #2C7744); /* Standard syntax */ } hr {color: red; }</style><link href="https://fonts.googleapis.com/css?family=Lato:400,400italic,700" rel="stylesheet" type="text/css">', err => {
 		if (err) {
 			return console.log(err);
 		}
@@ -56,6 +55,31 @@ function createMainWindow() {
 		win.loadURL(`file://${process.resourcesPath}/index2.html`);
 	});
 	return win;
+}
+
+// menu functions be here
+function funcLoad() {
+	const win = new electron.BrowserWindow({
+		width: 600,
+		height: 400
+	});
+	const loadFile = dialog.showOpenDialog({properties: ['openFile']});
+	win.loadURL(`file:///${loadFile}`);
+}
+function funcSave() {
+	dialog.showSaveDialog(fileName => {
+		if (fileName === undefined) {
+			console.log('You didn\'t save the file');
+			return;
+		}
+// fileName is a string that contains the path and filename created in the save file dialog.
+		fs.copy(`${process.resourcesPath}/index2.html`, fileName, err => {
+			if (err) {
+				console.log(err.message);
+			}
+		});
+	}
+);
 }
 
 app.on('window-all-closed', () => {
@@ -78,3 +102,145 @@ app.on('activate', () => {
 app.on('ready', () => {
 	mainWindow = createMainWindow();
 });
+
+const {Menu} = require('electron');
+
+const template = [
+	{
+		label: 'File',
+		submenu: [
+	{label: 'Save', click: funcSave},
+	{label: 'Open', click: funcLoad}
+
+		]
+	},
+	{
+		label: 'Edit',
+		submenu: [
+			{
+				role: 'selectall'
+			}
+		]
+	},
+	{
+		label: 'View',
+		submenu: [
+			{
+				label: 'Reload',
+				accelerator: 'CmdOrCtrl+R',
+				click(item, focusedWindow) {
+					if (focusedWindow) {
+						focusedWindow.reload();
+					}
+				}
+			},
+			{
+				role: 'togglefullscreen'
+			}
+		]
+	},
+	{
+		role: 'window',
+		submenu: [
+			{
+				role: 'minimize'
+			},
+			{
+				role: 'close'
+			}
+		]
+	},
+	{
+		role: 'help',
+		submenu: [
+			{
+				label: 'Learn More',
+				click() {
+					require('electron').shell.openExternal('http://electron.atom.io');
+				}
+			}
+		]
+	}
+];
+
+if (process.platform === 'darwin') {
+	const name = require('electron').remote.app.getName();
+
+	template.unshift({
+		label: name,
+		submenu: [
+			{
+				role: 'about'
+			},
+			{
+				type: 'separator'
+			},
+			{
+				role: 'services',
+				submenu: []
+			},
+			{
+				type: 'separator'
+			},
+			{
+				role: 'hide'
+			},
+			{
+				role: 'hideothers'
+			},
+			{
+				role: 'unhide'
+			},
+			{
+				type: 'separator'
+			},
+			{
+				role: 'quit'
+			}
+		]
+	});
+// Edit menu.
+	template[1].submenu.push(
+		{
+			type: 'separator'
+		},
+		{
+			label: 'Speech',
+			submenu: [
+				{
+					role: 'startspeaking'
+				},
+				{
+					role: 'stopspeaking'
+				}
+			]
+		}
+);
+// Window menu.
+	template[3].submenu = [
+		{
+			label: 'Close',
+			accelerator: 'CmdOrCtrl+W',
+			role: 'close'
+		},
+		{
+			label: 'Minimize',
+			accelerator: 'CmdOrCtrl+M',
+			role: 'minimize'
+		},
+		{
+			label: 'Zoom',
+			role: 'zoom'
+		},
+		{
+			type: 'separator'
+		},
+		{
+			label: 'Bring All to Front',
+			role: 'front'
+		}
+	];
+}
+
+const menu = Menu.buildFromTemplate(template);
+Menu.setApplicationMenu(menu);
