f69c3ff4116ada69c3d3a3c8213d84c0a3071b20
William <willyb321@users.noreply.github.com>
2016-12-15 12:01:40 +1100

    Log Watching Redux (#49)
    * Add lib/log-watcher.js (Thanks @sporebat)
    
    Next commits will be attempting to integrate it.
    
    * Fix debug not in deps.
    
    * right so i probably made progress implementing it
    
    * working on better watch.
    
    added an emitter when watcher finishes for the time being.
    
    * more progress on watching
    
    not much this time but its a bit of progress.
    
    * update deps.
    
    * should be nearly ready for release. still got some kinks though.
    
    * changed around the html gen and used proper names
    
    * woo I reckon this'll probably work pretty good.
    
    Now coords work and other things.
    
    * update deps
    
    * update readme
    
    * update deps
    
    * update deps, probably soon to be released
    
    * intellij
    
    * intellij
    
    * aaaaaaaaaaaaa
    
    * aaaaaaaaaaaaaaaaa
    
    * update deps
    
    * 1.8.0
    
    * docs update
    
    * report errors to bugsnag
    
    * 1.8.1
    
    * update ignore
    
    * probably fixed gitignore
    
    * testing opt-out
    
    * opt out test
    
    * 1.8.2
    
    * stuff
    
    * fix circleci builds
    
    * devlin pls
    
    * things
    
    * 1.8.3
    
    * fix xo lint
    
    * dont actually know.
    
    * fix unreachable codes
    
    * 1.8.4
    
    * fix finished emit.
    
    * fix sold exploration data list.
    
    * auto scroll to bottom in watch mode.
    
    * fix bits not being formatted properly
    
    * 1.8.5
    
    * probably fix .join not being a function.
    
    * 1.8.6
    
    * add loading message on watch
    
    * 1.8.7
    
    * semicolon...
    
    * add icon to loading.
    
    * 1.8.8

--- index.js
+++ index.js
@@ -14,9 +14,11 @@ const jsonfile = require('jsonfile');
 const bugsnag = require('bugsnag');
 const watch = require('node-watch');
 const openAboutWindow = require('about-window').default;
+const storage = require('electron-json-storage');
+const LogWatcher = require('./lib/log-watcher.js').LogWatcher;
 
-bugsnag.register('2ec6a43af0f3ef1f61f751191d6bd847');
 const app = electron.app;
+bugsnag.register('2ec6a43af0f3ef1f61f751191d6bd847', {appVersion: app.getVersion(), sendCode: true});
 let win;
 /** Autoupdater on update available */
 autoUpdater.on('update-available', info => { // eslint-disable-line no-unused-vars
@@ -28,7 +30,7 @@ autoUpdater.on('update-available', info => { // eslint-disable-line no-unused-va
 	});
 });
 /** Autoupdater on downloaded */
-autoUpdater.on('update-downloaded', releaseName => { // eslint-disable-line no-unused-vars
+autoUpdater.on('update-downloaded', () => { // eslint-disable-line no-unused-vars
 	dialog.showMessageBox({
 		type: 'info',
 		buttons: [],
@@ -44,7 +46,7 @@ autoUpdater.on('error', error => {
 		title: 'Update ready to install.',
 		message: `Sorry, we've had an error. The message is ` + error
 	});
-	if (!isDev) {
+	if (!isDev && uncaughtErr(error) !== {out: true}) {
 		bugsnag.notify(error);
 	}
 });
@@ -57,7 +59,7 @@ let JSONParsedEvent = [];
 let JSONParsed = []; // eslint-disable-line prefer-const
 const logPath = path.join(os.homedir(), 'Saved Games', 'Frontier Developments', 'Elite Dangerous');
 let htmlDone; // eslint-disable-line no-unused-vars
-const css = '<script src="https://use.fontawesome.com/a39359b6f9.js"></script><style>html, body{padding: 0;margin: 0;}#rectangle{width: 100%;height: 100%;background: red;}body{background-color: #313943;color: #bbc8d8;font-family: \'Lato\';font-size: 22px;font-weight: 500;line-height: 36px;margin-bottom: 36px;text-align: center;animation: fadein 0.5s;/* Cover the whole window */height: 100%;/* Make sure this matches the native window background color that you pass to * electron.BrowserWindow({...}), otherwise your app startup will look janky. */background: #313943;}header{position: absolute;width: 500px;height: 250px;top: 50%;left: 50%;margin-top: -125px;margin-left: -250px;text-align: center;}header h1{font-size: 60px;font-weight: 100;margin: 0;padding: 0;}#grad{background: -webkit-linear-gradient(left, #5A3F37, #2C7744);/* For Safari 5.1 to 6.0 */background: -o-linear-gradient(right, #5A3F37, #2C7744);/* For Opera 11.1 to 12.0 */background: -moz-linear-gradient(right, #5A3F37, #2C7744);/* For Firefox 3.6 to 15 */background: linear-gradient(to right, #5A3F37, #2C7744);/* Standard syntax */}hr{color: red;}@keyframes fadein{from{opacity: 0;}to{opacity: 1;}}.app{/* Disable text selection, or your app will feel like a web page */-webkit-user-select: none;-webkit-app-region: drag;/* Cover the whole window */height: 100%;/* Make sure this matches the native window background color that you pass to * electron.BrowserWindow({...}), otherwise your app startup will look janky. */background: #313943;/* Smoother startup */animation: fadein 0.5s;}</style><link href="https://fonts.googleapis.com/css?family=Lato:400,400italic,700" rel="stylesheet" type="text/css">';
+const css = '<link rel="stylesheet" href="./node_modules/izitoast/dist/css/iziToast.min.css"><script src="https://use.fontawesome.com/a39359b6f9.js"></script><style>html, body{padding: 0;margin: 0;}#rectangle{width: 100%;height: 100%;background: red;}body{background-color: #313943;color: #bbc8d8;font-family: \'Lato\';font-size: 22px;font-weight: 500;line-height: 36px;margin-bottom: 36px;text-align: center;animation: fadein 0.5s;/* Cover the whole window */height: 100%;/* Make sure this matches the native window background color that you pass to * electron.BrowserWindow({...}), otherwise your app startup will look janky. */background: #313943;}header{position: absolute;width: 500px;height: 250px;top: 50%;left: 50%;margin-top: -125px;margin-left: -250px;text-align: center;}header h1{font-size: 60px;font-weight: 100;margin: 0;padding: 0;}#grad{background: -webkit-linear-gradient(left, #5A3F37, #2C7744);/* For Safari 5.1 to 6.0 */background: -o-linear-gradient(right, #5A3F37, #2C7744);/* For Opera 11.1 to 12.0 */background: -moz-linear-gradient(right, #5A3F37, #2C7744);/* For Firefox 3.6 to 15 */background: linear-gradient(to right, #5A3F37, #2C7744);/* Standard syntax */}hr{display: flex}@keyframes fadein{from{opacity: 0;}to{opacity: 1;}}.app{/* Disable text selection, or your app will feel like a web page */-webkit-user-select: none;-webkit-app-region: drag;/* Cover the whole window */height: 100%;/* Make sure this matches the native window background color that you pass to * electron.BrowserWindow({...}), otherwise your app startup will look janky. */background: #313943;/* Smoother startup */animation: fadein 0.5s;}</style><link href="https://fonts.googleapis.com/css?family=Lato:400,400italic,700" rel="stylesheet" type="text/css">';
 // adds debug features like hotkeys for triggering dev tools and reload
 require('electron-debug')();
 // prevent window being garbage collected
@@ -82,6 +84,15 @@ function onClosed() {
 	// for multiple windows store them in an array
 	mainWindow = null;
 }
+function opted() {
+	storage.get('optOut', (err, data) => {
+		if (err) {
+			uncaughtErr(err);
+		}
+		return !(data.out === false || data.out === undefined);
+	});
+}
+
 /**
  * Used by various functions to show a dialog for loading files into the program
  */
@@ -103,11 +114,27 @@ function dialogLoad() {
 /**
  * On any uncaught exception notifys bugsnag and console logs the error.
  */
-process.on('uncaughtException', err => {
-	if (!isDev) {
-		bugsnag.notify(err);
-	}
+function uncaughtErr(err) {
+	storage.get('optOut', (error, data) => {
+		if (data) {
+			console.log(data);
+			if (data.out === false) {
+				bugsnag.notify(err);
+				return data;
+			} else if (data.out === true) {
+				dialog.showErrorBox('Error!', 'Please report the following: \n' + err);
+				return data;
+			}
+		}
+		if (error) {
+			console.log(error);
+		}
+	});
 	console.log('ERROR! The error is: ' + err.message);
+}
+
+process.on('uncaughtException', err => {
+	uncaughtErr(err);
 });
 /**
  * This is used for filtering.
@@ -156,6 +183,49 @@ function sortaSorter() {
 		});
 	}
 }
+function optOut(yes) {
+	storage.get('optOut', (error, data) => {
+		if (data) {
+			console.log(data);
+			if (data.out === false) {
+				console.log('in');
+			} else if (data.out === true) {
+				console.log('out');
+			}
+		}
+		if (error) {
+			console.log(error);
+		}
+	});
+	if (yes === 1) {
+		storage.set('optOut', {out: true}, err => {
+			if (err) {
+				console.log(err);
+			}
+			dialog.showMessageBox({
+				type: 'info',
+				buttons: [],
+				title: 'Opted back into diagnostics',
+				message: 'You have opted out of auto crash/error reporting.'
+			});
+			yes = undefined;
+		});
+	} else if (yes === 0) {
+		storage.set('optOut', {out: false}, err => {
+			if (err) {
+				console.log(err);
+				bugsnag.notify(new Error(err));
+			}
+			dialog.showMessageBox({
+				type: 'info',
+				buttons: [],
+				title: 'Opted back into diagnostics',
+				message: 'You have opted into auto crash/error reporting.'
+			});
+		});
+	}
+}
+
 /**
  * Used to populate the JSONParsedEvent array, which is used to load filtered logs.
  */
@@ -210,7 +280,7 @@ function lineReader(loadFile, html, watching) { // eslint-disable-line no-unused
 			process.logLoaded = true;
 			loadFile = '';
 		}
-	}) // eslint-disable-line semi
+	}); // eslint-disable-line semi
 }
 
 /**
@@ -238,10 +308,10 @@ function loadInit() {
 	loadAlternate(logorJSON, loadFile, html);
 }
 /**
- * @param  {String}
- * @param  {Array}
- * @param  {String}
  * Figures out how to load the file that was selected in loadInit()
+ * @param logorJSON
+ * @param loadFile
+ * @param html
  */
 function loadAlternate(logorJSON, loadFile, html) {
 	watching = false;
@@ -363,7 +433,7 @@ function loadOutputDropped() {
  * @param  {Boolean} stop - whether or not to stop watching logs.
  * @description Temporary, used to watch logs. A better solution is being developed.
  */
-function watchFor(stop) {
+function watchFor(stop) { // eslint-disable-line no-unused-vars
 	watching = true;
 	watcher = watch(logPath);
 	watcher.on('change', log => {
@@ -425,6 +495,56 @@ function funcSaveJSON() {
 		});
 	}
 }
+function watchGood(stop) {
+	process.logLoaded = true;
+	const watcher = new LogWatcher();
+	process.mainContents.executeJavaScript(`const iziToast = require('izitoast'); iziToast.show({title: 'Loading!', message: 'Please wait!', position: 'bottomCenter', image: 'icon.png', timeout: '10000'})`);
+	watcher.on('error', err => {
+		bugsnag.notify(err);
+	});
+	watcher.on('finished', () => {
+		console.log('it stopped');
+		JSONParsed = JSONParsed.reverse();
+		process.htmlDone = process.htmlDone.replace('undefined', '');
+		win.loadURL('data:text/html,' + css + stopdrop + `<script>function scroll() {window.scrollTo(0, document.body.scrollHeight || document.documentElement.scrollHeight);} window.onload = scroll;</script>` + process.htmlDone);
+		// JSONParsed = [];
+		// process.htmlDone = '';
+	});
+
+	watcher.on('stopped', () => {
+		console.log('nah its stopped');
+	});
+	watcher.on('data', obs => {
+		// obs = obs.reverse();
+		obs.forEach(ob => {
+			const {timestamp, event} = ob;
+			JSONParsed.push('\n' + event, timestamp); // eslint-disable-line no-useless-concat
+			process.htmlDone += '<hr>' + tableify(event, timestamp);
+			console.log('\n' + timestamp, event);
+			delete ob.timestamp;
+			delete ob.event;
+			Object.keys(ob).forEach(k => {
+				if (k === 'StarPos') {
+					process.htmlDone += '(x / y / z) <br>' + tableify(ob[k].join('<br>')) + '<br>';
+				} else if (k === 'Systems') {
+					process.htmlDone += '<br>Systems Sold: <br>' + tableify(ob[k].join('<br>')) + '<br>';
+				} else if (typeof ob[k] === 'object') {
+					let objtoarr = _.allKeys(ob[k]); // eslint-disable-line prefer-const
+					process.htmlDone += '<br>' + k + tableify(objtoarr.join('<br>')) + '<br>';
+				} else {
+					process.htmlDone += tableify(k) + ': ' + tableify(ob[k]) + '<br>';
+					console.log('\t' + k, ob[k]);
+					JSONParsed.push(k + ': ' + ob[k]);
+				}
+			});
+		});
+	});
+	if (stop === 1) {
+		watcher.stop();
+		process.logLoaded = false;
+	}
+}
+
 /**
  * Called when all windows are closed.
  */
@@ -456,8 +576,10 @@ ipcMain.on('asynchronous-drop', (event, arg) => {
  * Called when app is ready, and checks for updates.
  */
 app.on('ready', () => {
+	opted();
 	mainWindow = createMainWindow();
 	win.loadURL(`file:///${__dirname}/index.html`);
+	// watchGood();
 	if (!isDev) {
 		autoUpdater.checkForUpdates();
 	}
@@ -486,12 +608,12 @@ const template = [{
 		type: 'checkbox',
 		id: 'checked',
 		click(checked) {
-			let stop;
+			const stop = 1;
 			console.log(checked.checked);
 			if (checked.checked === true) {
-				watchFor();
+				watchGood();
 			} else if (checked.checked === false) {
-				watchFor(stop);
+				watchGood(stop);
 			}
 		}
 	}]
@@ -556,9 +678,23 @@ const template = [{
 			bug_report_url: 'https://github.com/willyb321/elite-journal/issues', // eslint-disable-line camelcase
 			homepage: 'https://github.com/willyb321/elite-journal'
 		})
+	}, {
+		label: 'Opt-out of auto crash reporting.',
+		type: 'checkbox',
+		id: 'optout',
+		checked: opted,
+		click: optout => {
+			let yes;
+			if (optout.checked === false) {
+				yes = 0;
+				optOut(yes);
+			} else if (optout.checked === true) {
+				yes = 1;
+				optOut(yes);
+			}
+		}
 	}
 	]
 }];
-
 const menu = Menu.buildFromTemplate(template);
 Menu.setApplicationMenu(menu);
